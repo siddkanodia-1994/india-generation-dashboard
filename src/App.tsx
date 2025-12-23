41b728a01a2fd29f7850ccabc990f721e2b94ea2
siddkanodia-1994 <siddkanodia@gmail.com>
2025-12-23 10:12:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -357,7 +357,7 @@ export default function App() {
   const [showYoYSeries, setShowYoYSeries] = useState(true);
   const [showMoMSeries, setShowMoMSeries] = useState(true);
 
-  const fileRef = useRef noting<File>();
+  const fileRef = useRef<HTMLInputElement | null>(null);
 
   const sortedDaily = useMemo<DailyPoint[]>(() => {
     return Array.from(dataMap.entries())
